fe77cb3b273f4f75866c624c4388030db2354d07
waseemwas <waseem.goa@gmail.com>
2026-09-03 16:30:02 +0100

    Update Lilly Monthly Billing data for eTMF, Collaborate & GL.sql

--- Scripts/Lilly Monthly Billing data for eTMF, Collaborate & GL.sql
+++ Scripts/Lilly Monthly Billing data for eTMF, Collaborate & GL.sql
@@ -27,3 +27,39 @@ WHERE ISNULL(umv.RoleLevel, 0) < 500
 
 
 ------Add the SiteID as needed-------
+
+-------Updated one---------
+
+SELECT 
+    sud.SiteId AS [Room Id], 
+    sud.SiteName AS [Room Name],
+    [$$InvitedOn$$] AS [Invited On], 
+    sud.FullName AS [User Full Name], 
+    sud.UserName AS [User Email Address], 
+    [$$InvitedBy$$] AS [Invited By],
+    [$$OrganizationName$$] AS [Organization Name], 
+    [$$RoleName$$] AS [Current Access Level],
+    [$$Actions$$] AS [Actions]
+
+FROM vw_SiteUserData sud
+
+INNER JOIN dbo.[User] u 
+    ON u.UserId = sud.UserId
+
+INNER JOIN dbo.UserMetadataVirtual umv 
+    ON umv.UserId = u.UserId 
+    AND umv.SiteId = sud.SiteId
+
+WHERE ISNULL(umv.RoleLevel, 0) < 500
+  AND sud.SiteName <> 'System'
+  AND [$$InvitedBy$$] <> 'API Service'
+  AND sud.UserName NOT LIKE '%contact.ct%'
+  AND NOT EXISTS (
+        SELECT *
+        FROM dbo.[InvestigativeSiteContacts] isc 
+        WHERE isc.SiteID = sud.SiteId 
+          AND isc.ContactId = sud.UserId
+    )
+  AND sud.SiteId BETWEEN 10 AND 300;
+
+------ Add the SiteID as needed ------
